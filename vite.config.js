d770c7028fae527300ded4499783ff35ae069250
Pibanezl <ped.ibanez@duocuc.cl>
2025-07-18 23:09:45 +1200

    second commit ever

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/3D_Portfolio/",
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/3D_Portfolio/' : '',
   plugins: [react()],
-})
+}));
